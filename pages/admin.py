2d29dbb374f134f127c80801126ec3b1fba0bc63
Suetone Carneiro <suetoneneto12@gmail.com>
2024-11-01 10:02:43 -0300

    :lipstick: feat: criação dos models da aplicação

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Usuario, Livro, Emprestimo, Contem
 
 # Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Livro)
+admin.site.register(Emprestimo)
+admin.site.register(Contem)
