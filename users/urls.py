138ab39b425600a218f7cfd437da42fec3509443
Suetone Carneiro <suetoneneto12@gmail.com>
2024-11-02 18:57:12 -0300

    :bug: fix: consertando a funcionalidade do logout

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login_leitor.html'), name='login'),
     path('login-admin/', auth_views.LoginView.as_view(template_name='users/login_adm.html'), name='login-admin'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
