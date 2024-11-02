64b94959ba3c5beb58b1b4d5ecb85dae5d00803e
Suetone Carneiro <suetoneneto12@gmail.com>
2024-11-02 11:03:35 -0300

    :lipstick: feat: adicionando obrigatoriedade de autenticação para acesso à biblioteca

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
-class BibliotecaView(TemplateView):
+class BibliotecaView(LoginRequiredMixin ,TemplateView):
+    login_url = reverse_lazy('login') # apenas usuários logados acessam a biblioteca
     template_name = 'biblioteca/biblioteca.html'
