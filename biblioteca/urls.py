b08f46742d123d8f691ced68b52a6ba3c0b13c2c
Suetone Carneiro <suetoneneto12@gmail.com>
2024-11-03 19:11:57 -0300

    :lipstick: feat: renderização dos livros na biblioteca e criação das próximas páginas a serem desenvolvidas

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import BibliotecaView
+from .views import BibliotecaView, EmprestimoView, HistoricoView, AdmView
 
 urlpatterns = [
     path('biblioteca/', BibliotecaView.as_view(template_name='biblioteca/biblioteca.html'), name='biblioteca'),
+    path('emprestimo/', EmprestimoView.as_view(template_name='biblioteca/form-emprestimo.html'), name='emprestimo'),
+    path('historico/', HistoricoView.as_view(template_name='biblioteca/historico-usuario.html'), name='historico'),
+    path('administrador/', AdmView.as_view(template_name='biblioteca/admin-area.html'), name='administracao'),
+
 ]
--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from pages.models import Livro
 from django.views.generic import TemplateView
 
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
-class BibliotecaView(LoginRequiredMixin ,TemplateView):
+class BibliotecaView(LoginRequiredMixin, ListView):
     login_url = reverse_lazy('login') # apenas usuários logados acessam a biblioteca
     template_name = 'biblioteca/biblioteca.html'
+    model = Livro # Listar elementos do meu banco de dados - nesse caso, os livros
+
+class EmprestimoView(LoginRequiredMixin, TemplateView):
+    login_url = reverse_lazy('login')
+    template_name= 'biblioteca/form-emprestimo.html'
+
+
+class HistoricoView(LoginRequiredMixin, TemplateView):
+    login_url = reverse_lazy('login')
+    template_name= 'biblioteca/historico-usuario.html'
+
+class AdmView(LoginRequiredMixin, TemplateView):
+    login_url = reverse_lazy('login')
+    template_name= 'biblioteca/admin-area.html'
+
