82075137320c8aedcb4e637e1f2f7bbe2272647a
Suetone Carneiro <suetoneneto12@gmail.com>
2024-11-04 20:37:26 -0300

    :lipstick: feat: criação da página de cadastro de usuários

---
+++ users/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UsuarioForm(UserCreationForm):
+    email = forms.EmailField()
+    endereco = forms.CharField(max_length=255, required=False, label='Endereço')
+    telefone = forms.CharField(max_length=22, required=False, label='Telefone')
+
+    class Meta:
+        model = User 
+        fields = ['username', 'email', 'endereco', 'telefone','password1', 'password2', 'is_staff']
--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
-
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .forms import UsuarioForm
 # Create your views here.
+
+class CadUsuarioView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    login_url = reverse_lazy('login')
+    template_name = 'biblioteca/cadastro-usuarios.html'
+    form_class = UsuarioForm
+    success_url = reverse_lazy('biblioteca')
+
+    def test_func(self): # permite apenas superusers nessa página
+        return self.request.user.is_superuser
+    
+    def handle_no_permission(self):
+        # Redireciona o usuário para a biblioteca, caso esteja logado
+        if self.request.user.is_authenticated:
+            return redirect('biblioteca') 
+        else:
+            # se não estiver logado, redireciona para login
+            return redirect(self.login_url)
